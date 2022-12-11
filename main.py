57e1d50d1684de7ebf142a8eb4f336b2333425c2
as39248 <113081162+as39248@users.noreply.github.com>
2022-12-11 01:21:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import discord
 intents = discord.Intents.all()
 intents.messages = True
 client = discord.Client(intents=intents)
-ban_list = ['https://images-ext-2.discordapp.net/external/oI2sB14yxXD8A5xKzRA2vt-XukTmKKlFiaQSyBVkBGI/https/im4.ezgif.com/tmp/ezgif-4-b9ea69a658.gif',
-            'https://media.discordapp.net/attachments/1000878710041366598/1049200300868767764/caption_6.gif',
-           'https://media.discordapp.net/attachments/1000878710041366598/1047281576150577192/20220410_141837.gif',
-            'https://media.discordapp.net/attachments/1000878710041366598/1043123510274424832/image0-211.gif',
-           'https://media2.giphy.com/media/E8M5aOqkzChemXhXwY/giphy.mp4?cid=73b8f7b15e53b4c2647cdeba781d2519bea91ede46ea87f0&rid=giphy.mp4&ct=g']
+# add contents that you want to ban in the list in String form
+ban_list = []
 
 
 @client.event
@@ -24,6 +21,5 @@ async def on_message(message):
     file1.close()
     print(f'{username} sent: "{sent_content}" in {channel}')
 
-
-client.run(
-    'MTA1MTI5NjQwMDk0OTkxOTgzNA.GWCM9u.upjg6ewcSXIHcKY2z5Kr8wzCvluHQkeO1FyU8g')
+# add discord bot token 
+client.run()
